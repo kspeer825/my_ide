14baa571d7cf3f99ae8a5b1c8fc84345d519b4a7
kspeer <kspeer@notreallymyemail.com>
2022-09-05 16:41:34 +0000

    fix setup check

--- setup_health.sh
+++ setup_health.sh
@@ -4,15 +4,15 @@
 
 
 # Homebrew
-command -v brew >/dev/null 2>&1 || echo "❌ Homebrew is missing. " && exit
+command -v brew >/dev/null 2>&1 || echo "❌ Homebrew is missing. "
 echo "✅ $(brew --version | head -1)"
 
 # Emacs
-command -v emacs >/dev/null 2>&1 || echo "❌ Emacs is missing. " && exit
+command -v emacs >/dev/null 2>&1 || echo "❌ Emacs is missing. "
 echo "✅ $(emacs --version | head -1)"
 
 # tmux
-command -v tmux >/dev/null 2>&1 || echo "❌ tmux is missing. " && exit
+command -v tmux >/dev/null 2>&1 || echo "❌ tmux is missing. "
 echo "✅ $(tmux -V)"
 
 echo "All Checks Passed."
